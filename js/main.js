8e9a01d3fd1add5481670b26dbbe0541c0d1ee28
yuji-web-developer <60169552+yuji-web-developer@users.noreply.github.com>
2020-01-26 13:56:44 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function( $ ) {
+
+  $(window).scroll(function() {
+    $('.si').each(function() {
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 400) {
+        $(this).addClass("slideUp");
+      }
+    });
+  });
+
+ 
+
+});
+
+
+
+
+
